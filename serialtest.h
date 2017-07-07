5a5cad71c34d0e851ee1e279818ff2118f9f32d1
zhangliang7190 <zl7190@126.com>
2017-07-07 20:39:19 +0800

    Add files via upload

---
+++ serialtest.h
@@ -0,0 +1,22 @@
+#ifndef SERIALTEST_H
+#define SERIALTEST_H
+#include "serial.h"
+
+#define REC_DATA_LEN  512
+class SerialTest
+{
+public:
+    SerialTest();
+    ~SerialTest();
+    int ValueInitOpenSerial(int index);
+
+    int Write();
+    int Read();
+private:
+    Serial *m_serial;
+    int m_serial_fd;
+    pthread_t m_rec_threadid;
+    unsigned char m_rec_data[REC_DATA_LEN];
+};
+
+#endif // SERIALTEST_H
